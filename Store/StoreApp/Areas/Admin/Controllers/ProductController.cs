6064d928331e249ac67b2c0e223728e7194434cc
cembora19 <cemboraceylandev@gmail.com>
2025-09-19 14:01:28 +0300

    IForm file and _images added to project

--- Store/StoreApp/Areas/Admin/Controllers/ProductController.cs
+++ Store/StoreApp/Areas/Admin/Controllers/ProductController.cs
@@ -31,6 +31,27 @@ namespace StoreApp.Areas.Admin.controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([FromForm] ProductDtoForInsertion productDto, IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                //file operation
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                productDto.ImageUrl = String.Concat("/images/", file.FileName);
+                _manager.ProductService.CreateProduct(productDto);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
         private SelectList GetCategoriesSelectList()
         {
             return new SelectList(_manager.CategoryService.GetAllCategories(false),
@@ -38,18 +59,6 @@ namespace StoreApp.Areas.Admin.controllers
             "CategoryName",
             "1"
             );
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create([FromForm] ProductDtoForInsertion productDto)
-        {
-            if (ModelState.IsValid)
-            {
-                _manager.ProductService.CreateProduct(productDto);
-                return RedirectToAction("Index");
-            }
-            return View();
         }
 
         public IActionResult Update([FromRoute(Name = "id")] int id)
@@ -61,11 +70,20 @@ namespace StoreApp.Areas.Admin.controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Update([FromForm]ProductDtoForUpdate product)
+        public async Task<IActionResult> Update([FromForm] ProductDtoForUpdate productDto, IFormFile file)
         {
             if (ModelState.IsValid)
             {
-                _manager.ProductService.UpdateOneProduct(product);
+                //file operation
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", file.FileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                productDto.ImageUrl = String.Concat("/images/", file.FileName);
+                _manager.ProductService.UpdateOneProduct(productDto);
                 return RedirectToAction("Index");
             }
             return View();
